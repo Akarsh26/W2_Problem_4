cf26bf4b93c0847efb1bd217319073befd430c4d
user <user@DESKTOP-NE5FF7L>
2020-12-13 15:09:22 +0530

    Created a data dictionary for design data for the controller

--- W2_P4.m
+++ W2_P4.m
@@ -7,8 +7,8 @@ s = tf('s');
 P_motor = K/(s*((J*s+b)*(L*s+R)+K^2));
 
 Kp = 21;
-Ki = 500;
-Kd = 0.05;
+Ki = 100;
+Kd = 0.15;
 
 for i = 1:3
     C(:,:,i) = pid(Kp,Ki,Kd);
@@ -19,5 +19,5 @@ sys_cl = feedback(C*P_motor,1);
 t = 0:0.001:0.1;
 step(sys_cl(:,:,1), sys_cl(:,:,2), sys_cl(:,:,3), t)
 ylabel('Position, \theta (radians)')
-title('Step Response with K_p = 21, K_i = 500 and Different Values of K_d')
+title('Step Response with K_p = 21, K_i = 100 and Different Values of K_d')
 legend('Kd = 0.05', 'Kd = 0.15', 'Kd = 0.25')
